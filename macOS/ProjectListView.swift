68c23753fbe2a11d82bf8010dd904b054b853359
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-01 18:10:35 -0400

    Build an initial version of the project list view on Mac.

--- macOS/ProjectListView.swift
+++ macOS/ProjectListView.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct ProjectListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Project.name, ascending: true)],
+        animation: .default)
+    private var projects: FetchedResults<Project>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(projects) { project in
+            Text("\(project.name!)")
+        }
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        ProjectListView()
     }
 }
