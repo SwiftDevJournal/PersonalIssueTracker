680bafe018511e134c2d571dfef3a8c5532a0455
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-02 16:33:09 -0400

    Fill the iOS version of the project list view with the code from the Mac version.

--- iOS/ProjectListView.swift
+++ iOS/ProjectListView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ProjectListView: View {
+    @State private var showAddSheet = false
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Project.name, ascending: true)],
+        animation: .default)
+    private var projects: FetchedResults<Project>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack {
+            Text("Projects")
+                .font(.title)
+            List(projects) { project in
+                Text("\(project.name!)")
+            }
+            Button(action: { showAddSheet = true }, label: {
+                Label("Add", systemImage: "note.text.badge.plus")
+            })
+        }
+        .sheet(isPresented: $showAddSheet) {
+            AddProjectView()
+        }
 }
 
 struct ProjectListView_Previews: PreviewProvider {
