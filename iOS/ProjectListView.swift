04f5aef7a2ebd97e4e1201ecd81011672ba98b03
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-02 16:28:46 -0400

    Add a file for the iOS version of the project list view.

---
+++ iOS/ProjectListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectListView.swift
+//  PersonalIssueTracker
+//
+//  Created by mark on 9/2/22.
+//
+
+import SwiftUI
+
+struct ProjectListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectListView()
+    }
+}
