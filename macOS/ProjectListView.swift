537ce4cd14bf13d4cafc2cae621e9843cfefa116
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-02 15:54:40 -0400

    Add buttons to add and remove projects to the project list view.
    Currently there isn't any code to add or remove projects. I've only laid out the interface.

--- macOS/ProjectListView.swift
+++ macOS/ProjectListView.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct ProjectListView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    @State private var showAddSheet = false
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Project.name, ascending: true)],
         animation: .default)
     private var projects: FetchedResults<Project>
     
     var body: some View {
-        List(projects) { project in
-            Text("\(project.name!)")
+        VStack {
+            Text("Projects")
+                .font(.title)
+            List(projects) { project in
+                Text("\(project.name!)")
+            }
+            HStack {
+                Button(action: { showAddSheet = true }, label: {
+                    Label("Add", systemImage: "note.text.badge.plus")
+                })
+
+                Spacer()
+
+                Button(action: {  }, label: {
+                    Label("Delete", systemImage: "trash")
+                        .foregroundColor(.red)
+                })
+                    .keyboardShortcut(.delete, modifiers: [])
+            }
+            .padding(.horizontal, 6)
+            .padding(6)
         }
+        .sheet(isPresented: $showAddSheet) {
+            AddProjectView()
+        }
+        
     }
 }
 
