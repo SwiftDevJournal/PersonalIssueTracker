a5ba80004636e6915867554d75585b312818a120
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-02 21:54:53 -0400

    Add a file for the issue list view.

---
+++ Shared/IssueListView.swift
@@ -0,0 +1,20 @@
+//
+//  IssueListView.swift
+//  PersonalIssueTracker
+//
+//  Created by mark on 9/2/22.
+//
+
+import SwiftUI
+
+struct IssueListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueListView_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueListView()
+    }
+}
