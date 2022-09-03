a8dbb1dccc7a2c37e72d1af69f0538bcd95b161d
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-03 14:40:45 -0400

    Add a file for the Add Issue view.

---
+++ Shared/AddIssueView.swift
@@ -0,0 +1,20 @@
+//
+//  AddIssueView.swift
+//  PersonalIssueTracker
+//
+//  Created by mark on 9/3/22.
+//
+
+import SwiftUI
+
+struct AddIssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddIssueView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIssueView()
+    }
+}
