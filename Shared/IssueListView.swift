0626fae865399600347a36fb9daa8f80ddd8c3f4
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-02 22:07:19 -0400

    Add an initial version of the issue list view.

--- Shared/IssueListView.swift
+++ Shared/IssueListView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct IssueListView: View {
+    @Binding var project: Project
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Issues")
+                .font(.title)
+            List(issueArray) { issue in
+                Text(issue.title ?? "")
+            }
+        }
+    }
+    
+    var issueArray: [Issue] {
+        return project.issues?.allObjects as? [Issue] ?? []
     }
 }
 
 struct IssueListView_Previews: PreviewProvider {
     static var previews: some View {
-        IssueListView()
+        IssueListView(project: .constant(Project()))
     }
 }
