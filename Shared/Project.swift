fa5a44976ee5568b3ec45645fb9fe99e558d2183
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-03 14:25:18 -0400

    Add a class extension file for the Project entity.

---
+++ Shared/Project.swift
@@ -0,0 +1,10 @@
+//
+//  Project.swift
+//  PersonalIssueTracker
+//
+//  Created by mark on 9/3/22.
+//
+
+import Foundation
+
+// I used Xcode's automatic class generation to generate NSManagedObject subclasses for the Core Data entities. The subclass files do not appear in Xcode's project navigator. I'm creating this file in case I need to add extensions to the Project class and to explain why there isn't a class definition for the Project entity.
