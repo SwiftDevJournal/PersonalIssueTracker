efc9e83dbc89a74cf3329a9f64edf17cc2866216
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-03 14:51:40 -0400

    Add buttons to add and close an issue to the issue list view.

--- Shared/IssueListView.swift
+++ Shared/IssueListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct IssueListView: View {
     @StateObject var project: Project
+    @State private var showAddSheet = false
+    @State private var selection: Issue? = nil
     
     var body: some View {
         VStack {
@@ -17,11 +19,35 @@ struct IssueListView: View {
             List(issueArray) { issue in
                 Text(issue.title ?? "")
             }
+            HStack {
+                Button(action: { showAddSheet = true }, label: {
+                    Label("Add", systemImage: "note.text.badge.plus")
+                })
+
+                Spacer()
+
+                // How do I get the selected item to the delete them.
+                Button(action: {
+                    closeIssue(selection!)
+                }, label: {
+                    Label("Close", systemImage: "trash")
+                        .foregroundColor(.red)
+                })
+            }
+            .padding(.horizontal, 6)
+            .padding(6)
+        }
+        .sheet(isPresented: $showAddSheet) {
+            AddIssueView(project: project)
         }
     }
     
     var issueArray: [Issue] {
         return project.issues?.allObjects as? [Issue] ?? []
+    }
+    
+    func closeIssue(_ issue: Issue) {
+        issue.open = false
     }
 }
 
