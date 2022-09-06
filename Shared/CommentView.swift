e8a04aa8bc71c4b4b9a3f9add139c9339f60f717
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-06 13:51:34 -0400

    Fill the comment view.
    Add a function to deal with SwiftUI bindings to optional values.

--- Shared/CommentView.swift
+++ Shared/CommentView.swift
@@ -7,14 +7,24 @@
 
 import SwiftUI
 
+// I need this function to deal with the comment possibly being nil. If the comment text is nil, place a default value in the text editor.
+func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    Binding(
+        get: { lhs.wrappedValue ?? rhs },
+        set: { lhs.wrappedValue = $0 }
+    )
+}
+
 struct CommentView: View {
+    @State var comment: Comment
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextEditor(text: $comment.contents ?? "")
     }
 }
 
 struct CommentView_Previews: PreviewProvider {
     static var previews: some View {
-        CommentView()
+        CommentView(comment: Comment())
     }
 }
