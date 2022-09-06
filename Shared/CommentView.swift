aff5feb477eb3560f33753dfe83d3d6bbd6c38b8
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-06 13:33:16 -0400

    Add a comment view to the project.

---
+++ Shared/CommentView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentView.swift
+//  PersonalIssueTracker
+//
+//  Created by mark on 9/6/22.
+//
+
+import SwiftUI
+
+struct CommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentView()
+    }
+}
