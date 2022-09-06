7c7e252a6b89371cad2093906e3b7553680b7fc8
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-06 14:23:25 -0400

    Use the .columns navigation style on iOS.

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Project.name, ascending: true)],
+        animation: .default)
+    private var projects: FetchedResults<Project>
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ProjectListView()
+            EmptyView()
+            EmptyView()
+        }
+        .navigationViewStyle(.columns)
     }
 }
 
