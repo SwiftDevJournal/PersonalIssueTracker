4b157a2ed09fa24804c6b4978ab6f02d25d4e0c6
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-06 15:34:26 -0400

    Save the managed object context after closing an issue.
    The issue list doesn't refresh after closing the issue.

--- Shared/IssueListView.swift
+++ Shared/IssueListView.swift
@@ -11,6 +11,7 @@ struct IssueListView: View {
     @StateObject var project: Project
     @State private var showAddSheet = false
     @State private var selection: Issue? = nil
+    @Environment(\.managedObjectContext) private var viewContext
     
     var body: some View {
         VStack {
@@ -54,6 +55,15 @@ struct IssueListView: View {
     
     func closeIssue(_ issue: Issue) {
         issue.open = false
+        
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
 
