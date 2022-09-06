8e11df471b71df9c87fc3ddb00807d3f4af3a841
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-06 14:19:05 -0400

    Add a new content view for iOS. I have to explicitly use columns navigation to get a split view on iOS. But it kills macOS 11 compatibility so I'm creating separate content views.

---
+++ iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PersonalIssueTracker (iOS)
+//
+//  Created by mark on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
