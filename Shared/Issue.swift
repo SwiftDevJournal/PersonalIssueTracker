af6f73bc5235f3d9ed81bdede4c9897efe281adf
Mark Szymczyk <mark@swiftdevjournal.com>
2022-12-04 15:05:21 -0500

    Create an enum for the issue priorities.
    Add an extension to the Issue class to allow me to use the enum in my code instead of its raw value.

--- Shared/Issue.swift
+++ Shared/Issue.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import CoreData
+
+enum IssuePriority: Int {
+    case low = 1
+    case medium = 2
+    case high = 3
+}
+
+extension Issue {
+    @NSManaged private var priorityValue: Int
+    
+    var priority: IssuePriority {
+        get {
+            return IssuePriority(rawValue: self.priorityValue) ?? .low
+        }
+        set {
+            self.priorityValue = newValue.rawValue
+        }
+    }
+}
